ea5f0012f4a6b43537d9ae8d888a054be182e78b
stdll00 <16006732+stdll00@users.noreply.github.com>
2024-01-06 13:44:22 +0900

    fix: remove test

--- go.mod
+++ go.mod
@@ -2,10 +2,10 @@ module github.com/stdll00/grpc-web-go-client
 
 require (
 	github.com/google/go-cmp v0.6.0
-	github.com/ktr0731/grpc-test v0.1.7 // not works with 0.1.8
 	go.uber.org/atomic v1.11.0
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240102182953-50ed04b92917
 	google.golang.org/grpc v1.60.1
+	google.golang.org/grpc/examples v0.0.0-20240104213007-a233d9b577d8
 	google.golang.org/protobuf v1.32.0
 )
 
@@ -15,7 +15,6 @@ require (
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/grpc/examples v0.0.0-20240104213007-a233d9b577d8 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
 
--- grpcweb/grpcweb_test.go
+++ grpcweb/grpcweb_test.go
@@ -2,215 +2,57 @@ package grpcweb
 
 import (
 	"context"
+	"fmt"
+	"google.golang.org/grpc/examples/helloworld/helloworld"
 	"io"
 	"net/http"
-	"os"
-	"path/filepath"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
-	"github.com/ktr0731/grpc-test/api"
-	"github.com/stdll00/grpc-web-go-client/grpcweb/transport"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 )
 
-type unaryTransport struct {
-	t *testing.T
-
-	expectedMD metadata.MD
-	h          http.Header
-	r          io.ReadCloser
-	err        error
+// httpServer
+type httpServer struct {
+	lastRequest    []byte
+	lastHeader     http.Header
+	response       []byte
+	responseHeader http.Header
 }
 
-func (t *unaryTransport) Header() http.Header {
-	return make(http.Header)
-}
-
-func (t *unaryTransport) Send(ctx context.Context, endpoint, contentType string, body io.Reader) (http.Header, io.ReadCloser, error) {
-	md, ok := metadata.FromOutgoingContext(ctx)
-	if !ok {
-		t.t.Fatalf("outgoing ctx should have metadata")
+func (s *httpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Store the body of the request
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body",
+			http.StatusInternalServerError)
+		return
 	}
-	if diff := cmp.Diff(t.expectedMD, md); diff != "" {
-		t.t.Fatalf("-want, +got\n%s", diff)
+	s.lastRequest = body
+	// Store the header of the request
+	s.lastHeader = r.Header
+
+	// Set the response header
+	for key, value := range s.responseHeader {
+		for _, v := range value {
+			w.Header().Add(key, v)
+		}
 	}
-	return t.h, t.r, t.err
+	w.Write(s.response)
 }
 
-func (t *unaryTransport) Close() error {
-	return nil
-}
+func TestWithGeneratedClient(t *testing.T) {
+	addr := ":50051"
+	ctx := context.Background()
+	s := &httpServer{}
+	go http.ListenAndServe(addr, s)
 
-func TestInvoke(t *testing.T) {
-	header := http.Header{
-		"hakase": []string{"shinonome"},
-		"nano":   []string{"shinonome"},
+	dial, err := DialContext(addr)
+	if err != nil {
+		panic("err")
 	}
-
-	cases := map[string]struct {
-		transportHeader          http.Header
-		transportContentFileName string
-		expectedHeader           metadata.MD
-		expectedTrailer          metadata.MD
-		expectedContent          api.SimpleResponse
-		expectedStatus           *status.Status
-		wantErr                  bool
-	}{
-		"normal (only response)": {
-			transportHeader:          header,
-			transportContentFileName: "response.in",
-			expectedHeader: metadata.New(map[string]string{
-				"hakase": "shinonome",
-				"nano":   "shinonome",
-			}),
-			expectedContent: api.SimpleResponse{Message: "hello, ktr"},
-			expectedStatus:  status.New(codes.OK, ""),
-		},
-		"normal (trailer and response)": {
-			transportHeader:          header,
-			transportContentFileName: "trailer_response.in",
-			expectedHeader: metadata.New(map[string]string{
-				"hakase": "shinonome",
-				"nano":   "shinonome",
-			}),
-			expectedTrailer: metadata.New(map[string]string{
-				"trailer_key1": "trailer_val1",
-				"trailer_key2": "trailer_val2",
-			}),
-			expectedContent: api.SimpleResponse{Message: "response"},
-			expectedStatus:  status.New(codes.OK, ""),
-		},
-		"error (trailer and response)": {
-			transportHeader:          header,
-			transportContentFileName: "trailer_response_error.in",
-			expectedHeader: metadata.New(map[string]string{
-				"hakase": "shinonome",
-				"nano":   "shinonome",
-			}),
-			expectedTrailer: metadata.New(map[string]string{
-				"trailer_key1": "trailer_val1",
-				"trailer_key2": "trailer_val2",
-			}),
-			expectedStatus: status.New(codes.Internal, "internal error"),
-			wantErr:        true,
-		},
+	client := helloworld.NewGreeterClient(dial) // just check interface
+	_, err = client.SayHello(ctx, &helloworld.HelloRequest{Name: "hello"})
+	fmt.Print(s.lastRequest)
+	if err != nil {
+		return
 	}
-
-	for name, c := range cases {
-		t.Run(name, func(t *testing.T) {
-			r, err := os.Open(filepath.Join("testdata", c.transportContentFileName))
-			if err != nil {
-				t.Fatalf("Open should not return an error, but got '%s'", err)
-			}
-
-			md := metadata.Pairs("yuko", "aioi")
-
-			injectUnaryTransport(t, &unaryTransport{
-				t:          t,
-				expectedMD: md,
-				h:          c.transportHeader,
-				r:          r,
-			})
-
-			var header, trailer metadata.MD
-			client, err := DialContext(":50051")
-			if err != nil {
-				t.Fatalf("DialContext should not return an error, but got '%s'", err)
-			}
-
-			var res api.SimpleResponse
-			opts := []grpc.CallOption{grpc.Header(&header), grpc.Trailer(&trailer)}
-			req := api.SimpleRequest{Name: "nano"}
-			ctx := metadata.NewOutgoingContext(context.Background(), md)
-			err = client.Invoke(ctx, "/service/Method", &req, &res, opts...)
-			if c.wantErr {
-				if err == nil {
-					t.Fatalf("should return an error, but got nil")
-				}
-			} else if err != nil {
-				t.Fatalf("should not return an error, but got '%s'", err)
-			}
-
-			stat := status.Convert(err)
-
-			if diff := cmp.Diff(c.expectedHeader, header); diff != "" {
-				t.Errorf("-want, +got\n%s", diff)
-			}
-			if diff := cmp.Diff(c.expectedTrailer, trailer); diff != "" {
-				t.Errorf("-want, +got\n%s", diff)
-			}
-			if diff := cmp.Diff(c.expectedContent, res); diff != "" {
-				t.Errorf("-want, +got\n%s", diff)
-			}
-			if stat.Code() != c.expectedStatus.Code() {
-				t.Errorf("expected status code: %s, but got %s", c.expectedStatus.Code(), stat.Code())
-			}
-			if stat.Message() != c.expectedStatus.Message() {
-				t.Errorf("expected status message: %s, but got %s", c.expectedStatus.Message(), stat.Message())
-			}
-		})
-	}
-}
-
-func injectUnaryTransport(t *testing.T, tr transport.UnaryTransport) {
-	old := transport.NewUnary
-	t.Cleanup(func() {
-		transport.NewUnary = old
-	})
-	transport.NewUnary = func(string, *transport.ConnectOptions) transport.UnaryTransport {
-		return tr
-	}
-}
-
-type clientStreamTransport struct {
-	tt             *testing.T
-	expectedHeader http.Header
-
-	sentCloseSend bool
-
-	h, t http.Header
-	r    []io.ReadCloser
-	err  error
-
-	i int
-}
-
-func (s *clientStreamTransport) SetRequestHeader(h http.Header) {
-	if diff := cmp.Diff(s.expectedHeader, h); diff != "" {
-		s.tt.Fatalf("-want, +got\n%s", diff)
-	}
-}
-
-func (s *clientStreamTransport) Header() (http.Header, error) {
-	return s.h, nil
-}
-
-func (s *clientStreamTransport) Trailer() http.Header {
-	return s.t
-}
-
-func (s *clientStreamTransport) Send(context.Context, io.Reader) error {
-	return nil
-}
-
-func (s *clientStreamTransport) Receive(context.Context) (io.ReadCloser, error) {
-	if s.sentCloseSend && s.err != nil {
-		return nil, s.err
-	}
-	r := s.r[s.i]
-	s.i++
-	return r, nil
-}
-
-func (s *clientStreamTransport) CloseSend() error {
-	s.sentCloseSend = true
-	return nil
-}
-
-func (s *clientStreamTransport) Close() error {
-	return nil
 }
